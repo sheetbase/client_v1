b2af94bee5783fc02bebc222956579c2afbdaa87
Lam Nhan <lamnhan92@gmail.com>
2019-03-13 20:23:24 +0700

    add type

--- src/lib/auth/types.ts
+++ src/lib/auth/types.ts
@@ -1,3 +1,28 @@
 export interface AuthOptions {
     authEndpoint?: string;
 }
+
+export type ProviderId = 'password' | 'custom' | 'anonymous' | 'google.com' | 'facebook.com' | 'twitter.com';
+
+export interface UserInfo extends UserProfile {
+    '#'?: number;
+    uid?: string;
+    providerId?: ProviderId;
+    providerData?: any;
+    email?: string;
+    emailVerified?: boolean;
+    createdAt?: string;
+    lastLogin?: string;
+    username?: string;
+    phoneNumber?: string;
+    claims?: {
+        [claim: string]: any;
+    };
+    isAnonymous?: boolean;
+    isNewUser?: boolean;
+}
+
+export interface UserProfile {
+    displayName?: string;
+    photoURL?: string;
+}
