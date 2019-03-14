0055be9236c0c4e60095f4ee6927583bde1c220e
Lam Nhan <lamnhan92@gmail.com>
2019-03-14 20:38:14 +0700

    update query

--- src/lib/database/database.service.ts
+++ src/lib/database/database.service.ts
@@ -1,7 +1,7 @@
 import { AppService } from '../app/app.service';
 import { ApiService } from '../api/api.service';
 
-import { Filter, AdvancedFilter } from './types';
+import { Filter, AdvancedFilter, Query } from './types';
 
 export class DatabaseService {
 
@@ -35,28 +35,117 @@ export class DatabaseService {
             } else {
                 where = Object.keys(filter)[0];
                 equal = filter[where];
+                delete filter[where]; // remove shorthand value from filter
             }
         }
         // query items
         if (offline) {
+            // turn simple query into advanced query
+            if (!advancedFilter) {
+                // build advanced filter
+                const {
+                    exists,
+                    contains,
+                    lt, lte,
+                    gt, gte,
+                    childExists,
+                    childEqual,
+                } = filter as Query;
+                if (!!equal) { // where/equal
+                    advancedFilter = item => (!!item[where] && item[where] === equal);
+                } else if (typeof exists === 'boolean') { // where/exists/not exists
+                    advancedFilter = item => (!!exists ? !!item[where] : !item[where]);
+                } else if (!!contains) { // where/contains
+                    advancedFilter = item => (
+                        !!item[where] &&
+                        typeof item[where] === 'string' &&
+                        item[where].indexOf(contains) > -1
+                    );
+                } else if (!!lt) { // where/less than
+                    advancedFilter = item => (
+                        !!item[where] &&
+                        typeof item[where] === 'number' &&
+                        item[where] < lt
+                    );
+                } else if (!!lte) { // where/less than or equal
+                    advancedFilter = item => (
+                        !!item[where] &&
+                        typeof item[where] === 'number' &&
+                        item[where] <= lte
+                    );
+                } else if (!!gt) { // where/greater than
+                    advancedFilter = item => (
+                        !!item[where] &&
+                        typeof item[where] === 'number' &&
+                        item[where] > gt
+                    );
+                } else if (!!gte) { // where/greater than or equal
+                    advancedFilter = item => (
+                        !!item[where] &&
+                        typeof item[where] === 'number' &&
+                        item[where] >= gte
+                    );
+                } else if (!!childExists) { // where/child exists, not exists
+                    const notExists = childExists.substr(0, 1) === '!';
+                    const child = notExists ? childExists.replace('!', '') : childExists;
+                    advancedFilter = item => {
+                        if (!item[where] && notExists) {
+                            return true; // child always not exists
+                        } else if (!!item[where]) {
+                            if (item[where] instanceof Array) {
+                                return notExists ?
+                                    (item[where].indexOf(child) < 0) :
+                                    (item[where].indexOf(child) > -1);
+                            } else if (item[where] instanceof Object) {
+                                return notExists ? !item[where][child] : !!item[where][child];
+                            }
+                        }
+                        return false;
+                    };
+                } else if (!!childEqual) { // where/child equal, not equal
+                    let notEqual: boolean;
+                    let childKey: string;
+                    let childValue: string;
+                    if (childEqual.indexOf('!=') > -1) {
+                        notEqual = true;
+                        const keyValue = childEqual.split('!=').filter(Boolean);
+                        childKey = keyValue[0];
+                        childValue = keyValue[1];
+                    } else {
+                        const keyValue = childEqual.split('=').filter(Boolean);
+                        childKey = keyValue[0];
+                        childValue = keyValue[1];
+                    }
+                    advancedFilter = item => {
+                        if (!item[where] && notEqual) {
+                            return true; // always not equal
+                        } else if (!!item[where]) {
+                            return  (
+                                item[where] instanceof Object &&
+                                (notEqual ?
+                                    (!item[where][childKey] || item[where][childKey] !== childValue) :
+                                    (!!item[where][childKey] && item[where][childKey] === childValue)
+                                )
+                            );
+                        }
+                        return false;
+                    };
+                }
+            }
             // load local items
             const localItems = await this.all(sheet);
-            // turn where/equal to advance filter
-            advancedFilter = advancedFilter || ((item: Item) => {
-                return item[where] === equal;
-            });
             // query local items
             const items: Item[] = [];
             for (let i = 0; i < localItems.length; i++) {
                 const item = localItems[i] as Item;
-                if (advancedFilter(item)) {
+                if (!!advancedFilter(item)) {
                     items.push(item);
                 }
             }
             return items;
         } else {
             if (!advancedFilter) {
-                return await this.Api.get('/', { sheet, where, equal }, cacheTime);
+                return await this.Api.get('/', { sheet, where, equal, ... filter }, cacheTime);
             } else {
                 throw new Error('Can only apply advanced query when offline argument is true.');
             }
--- src/lib/database/types.ts
+++ src/lib/database/types.ts
@@ -2,10 +2,18 @@ export interface DatabaseOptions {
     databaseEndpoint?: string;
 }
 
+export type AdvancedFilter = (item: any) => boolean;
+export type ShorthandEqual = {[field: string]: any};
+export type Filter = ShorthandEqual | Query | AdvancedFilter;
 export interface Query {
     where: string;
     equal?: any;
+    exists?: true;
+    contains?: string;
+    lt?: number;
+    lte?: number;
+    gt?: number;
+    gte?: number;
+    childExists?: any;
+    childEqual?: string;
 }
-
-export type AdvancedFilter = (item: any) => boolean;
-export type Filter = {[field: string]: any} | Query | AdvancedFilter;
