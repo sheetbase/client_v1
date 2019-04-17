358f801d7cf89c59d605d6b754e1dd91ac7d64cd
Lam Nhan <lamnhan92@gmail.com>
2019-04-17 16:05:41 +0700

    update specs

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -5,10 +5,9 @@ import * as sinon from 'sinon';
 import 'jsdom-global/register';
 
 import { ApiService } from '../src/lib/api/api.service';
-import { AuthService } from '../src/lib/auth/auth.service';
-import { DatabaseService } from '../src/lib/database/database.service';
-import { StorageService } from '../src/lib/storage/storage.service';
-import { MailService } from '../src/lib/mail/mail.service';
+import { FetchService } from '../src/lib/fetch/fetch.service';
+import { LocalstorageService } from '../src/lib/localstorage/localstorage.service';
+import { CacheService } from '../src/lib/cache/cache.service';
 
 import { AppService, AppsService } from '../src/lib/app/app.service';
 import { initializeApp, defaultApp, app } from '../src/lib/app/index';
@@ -17,86 +16,96 @@ const OPTIONS = { backendUrl: '' };
 
 describe('(App) AppService', () => {
 
-    it('should be created', () => {
-        window['$$$SHEETBASE_COMPONENTS'] = {};
+  it('should be created', () => {
+    window['$$$SHEETBASE_COMPONENTS'] = {};
 
-        const sheetbaseApp = new AppService(OPTIONS);
-        expect(sheetbaseApp.options).to.eql(OPTIONS);
-        expect(sheetbaseApp.Api instanceof ApiService).to.equal(true, '.Api');
-        expect(sheetbaseApp.api() instanceof ApiService).to.equal(true, '#api');
-        expect(sheetbaseApp.auth.bind(sheetbaseApp)).to.throw('No auth component.', '#auth');
-        expect(sheetbaseApp.database.bind(sheetbaseApp)).to.throw('No database component.', '#database');
-        expect(sheetbaseApp.storage.bind(sheetbaseApp)).to.throw('No storage component.', '#storage');
-        expect(sheetbaseApp.mail.bind(sheetbaseApp)).to.throw('No mail component.', '#mail');
-    });
+    const sheetbaseApp = new AppService(OPTIONS);
 
-    it('should initialize other components', () => {
-        window['$$$SHEETBASE_COMPONENTS'] = {
-            Auth: AuthService,
-            Database: DatabaseService,
-            Storage: StorageService,
-            Mail: MailService,
-        };
+    expect(sheetbaseApp.options).to.eql(OPTIONS);
+    // built-in
+    expect(sheetbaseApp.Api instanceof ApiService).to.equal(true, '.Api');
+    expect(sheetbaseApp.api() instanceof ApiService).to.equal(true, '#api');
+    expect(sheetbaseApp.Fetch instanceof FetchService).to.equal(true, '.Fetch');
+    expect(sheetbaseApp.fetch() instanceof FetchService).to.equal(true, '#fetch');
+    expect(sheetbaseApp.Localstorage instanceof LocalstorageService).to.equal(true, '.Localstorage');
+    expect(sheetbaseApp.localstorage() instanceof LocalstorageService).to.equal(true, '#localstorage');
+    expect(sheetbaseApp.Cache instanceof CacheService).to.equal(true, '.Cache');
+    expect(sheetbaseApp.cache() instanceof CacheService).to.equal(true, '#cache');
+    // custom
+    expect(sheetbaseApp.auth.bind(sheetbaseApp)).to.throw('No auth component.', '#auth');
+    expect(sheetbaseApp.database.bind(sheetbaseApp)).to.throw('No database component.', '#database');
+    expect(sheetbaseApp.storage.bind(sheetbaseApp)).to.throw('No storage component.', '#storage');
+    expect(sheetbaseApp.mail.bind(sheetbaseApp)).to.throw('No mail component.', '#mail');
+  });
 
-        const sheetbaseApp = new AppService(OPTIONS);
-        expect(sheetbaseApp.Auth instanceof AuthService).to.equal(true, '.Auth');
-        expect(sheetbaseApp.Database instanceof DatabaseService).to.equal(true, '.Database');
-        expect(sheetbaseApp.Storage instanceof StorageService).to.equal(true, '.Storage');
-        expect(sheetbaseApp.Mail instanceof MailService).to.equal(true, '.Mail');
-        expect(sheetbaseApp.auth() instanceof AuthService).to.equal(true, '#auth');
-        expect(sheetbaseApp.database() instanceof DatabaseService).to.equal(true, '#database');
-        expect(sheetbaseApp.storage() instanceof StorageService).to.equal(true, '#storage');
-        expect(sheetbaseApp.mail() instanceof MailService).to.equal(true, '#mail');
-    });
+  it('should initialize other components', () => {
+    window['$$$SHEETBASE_COMPONENTS'] = {
+      Auth: class Faked {},
+      Database: class Faked {},
+      Storage: class Faked {},
+      Mail: class Faked {},
+    };
+
+    const sheetbaseApp = new AppService(OPTIONS);
+
+    expect(!!sheetbaseApp.Auth).to.equal(true, '.Auth');
+    expect(!!sheetbaseApp.Database).to.equal(true, '.Database');
+    expect(!!sheetbaseApp.Storage).to.equal(true, '.Storage');
+    expect(!!sheetbaseApp.Mail).to.equal(true, '.Mail');
+    expect(!!sheetbaseApp.auth()).to.equal(true, '#auth');
+    expect(!!sheetbaseApp.database()).to.equal(true, '#database');
+    expect(!!sheetbaseApp.storage()).to.equal(true, '#storage');
+    expect(!!sheetbaseApp.mail()).to.equal(true, '#mail');
+  });
 
 });
 
 describe('(App) AppsService', () => {
 
-    const appsService = new AppsService();
+  const appsService = new AppsService();
 
-    it('#createApp', () => {
-        appsService.createApp({ backendUrl: '' });
-        appsService.createApp({ backendUrl: '' }, 'app1');
-        appsService.createApp({ backendUrl: '' }, 'app2');
-        // @ts-ignore
-        expect(Object.keys(appsService.apps)).to.eql(['DEFAULT', 'app1', 'app2']);
-    });
+  it('#createApp', () => {
+    appsService.createApp({ backendUrl: '' });
+    appsService.createApp({ backendUrl: '' }, 'app1');
+    appsService.createApp({ backendUrl: '' }, 'app2');
+    // @ts-ignore
+    expect(Object.keys(appsService.apps)).to.eql(['DEFAULT', 'app1', 'app2']);
+  });
 
-    it('#createApp should throw error, app exists', () => {
-        expect(
-            appsService.createApp.bind(appsService, { backendUrl: '' }),
-        ).to.throw('An app exists with the name "DEFAULT".');
-    });
+  it('#createApp should throw error, app exists', () => {
+    expect(
+      appsService.createApp.bind(appsService, { backendUrl: '' }),
+    ).to.throw('An app exists with the name "DEFAULT".');
+  });
 
-    it('#getApp', () => {
-        const app2 = appsService.getApp('app2');
-        expect(!!app2).to.equal(true);
-    });
+  it('#getApp', () => {
+    const app2 = appsService.getApp('app2');
+    expect(!!app2).to.equal(true);
+  });
 
-    it('#getApp should throw error, app not exists', () => {
-        expect(
-            appsService.getApp.bind(appsService, 'app3'),
-        ).to.throw('No app exists with the name "app3". Please run initializeApp() first.');
-    });
+  it('#getApp should throw error, app not exists', () => {
+    expect(
+      appsService.getApp.bind(appsService, 'app3'),
+    ).to.throw('No app exists with the name "app3". Please run initializeApp() first.');
+  });
 
 });
 
 describe('(App) methods', () => {
 
-    it('#initializeApp', () => {
-        const sheetbaseApp = initializeApp(OPTIONS);
-        expect(sheetbaseApp instanceof AppService).to.equal(true);
-    });
+  it('#initializeApp', () => {
+    const sheetbaseApp = initializeApp(OPTIONS);
+    expect(sheetbaseApp instanceof AppService).to.equal(true);
+  });
 
-    it('#defaultApp', () => {
-        const sheetbaseApp = defaultApp();
-        expect(sheetbaseApp instanceof AppService).to.equal(true);
-    });
+  it('#defaultApp', () => {
+    const sheetbaseApp = defaultApp();
+    expect(sheetbaseApp instanceof AppService).to.equal(true);
+  });
 
-    it('#app', () => {
-        const sheetbaseApp = app('DEFAULT');
-        expect(sheetbaseApp instanceof AppService).to.equal(true);
-    });
+  it('#app', () => {
+    const sheetbaseApp = app('DEFAULT');
+    expect(sheetbaseApp instanceof AppService).to.equal(true);
+  });
 
 });
--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -7,62 +7,62 @@ global['atob'] = (b64: string) => Buffer.from(b64, 'base64').toString();
 
 describe('utils', () => {
 
-    it('#decodeJWTPayload', () => {
-        // tslint:disable-next-line:max-line-length
-        const result = decodeJWTPayload('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
-        expect(result).to.eql({
-            sub: '1234567890',
-            name: 'John Doe',
-            iat: 1516239022,
-        });
+  it('#decodeJWTPayload', () => {
+    // tslint:disable-next-line:max-line-length
+    const result = decodeJWTPayload('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+    expect(result).to.eql({
+      sub: '1234567890',
+      name: 'John Doe',
+      iat: 1516239022,
     });
+  });
 
-    it('#isExpiredJWT', () => {
-        const TOKEN1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.Et9HFtf9R3GEMA0IICOfFMVXY7kkTX1wr4qCyhIf58U';
-        const TOKEN2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-            Buffer.from(JSON.stringify({
-                exp: Math.ceil(new Date().getTime() / 1000), // expire now
-            }))
-            .toString('base64')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/\=/g, '') +
-            '.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+  it('#isExpiredJWT', () => {
+    const TOKEN1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.Et9HFtf9R3GEMA0IICOfFMVXY7kkTX1wr4qCyhIf58U';
+    const TOKEN2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+      Buffer.from(JSON.stringify({
+        exp: Math.ceil(new Date().getTime() / 1000), // expire now
+      }))
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/\=/g, '') +
+      '.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-        const TOKEN3 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-            Buffer.from(JSON.stringify({
-                exp: Math.ceil(new Date().getTime() / 1000) + 3600, // expire 1 hour from now
-            }))
-            .toString('base64')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/\=/g, '') +
-            '.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    const TOKEN3 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+      Buffer.from(JSON.stringify({
+        exp: Math.ceil(new Date().getTime() / 1000) + 3600, // expire 1 hour from now
+      }))
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/\=/g, '') +
+      '.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-        const result1 = isExpiredJWT(TOKEN1);
-        const result2 = isExpiredJWT(TOKEN2);
-        const result3 = isExpiredJWT(TOKEN3);
-        expect(result1).to.equal(true, 'no exp');
-        expect(result2).to.equal(true, 'expired');
-        expect(result3).to.equal(false, 'not expired');
-    });
+    const result1 = isExpiredJWT(TOKEN1);
+    const result2 = isExpiredJWT(TOKEN2);
+    const result3 = isExpiredJWT(TOKEN3);
+    expect(result1).to.equal(true, 'no exp');
+    expect(result2).to.equal(true, 'expired');
+    expect(result3).to.equal(false, 'not expired');
+  });
 
-    it('#ApiError', () => {
-        const error = { error: true, code: 'xxx', message: 'Route error ...' };
-        const result = new ApiError(error);
-        expect(result.name).to.equal('ApiError');
-        expect(result.message).to.equal('Route error ...');
-        expect(result.error).to.eql(error);
-    });
+  it('#ApiError', () => {
+    const error = { error: true, code: 'xxx', message: 'Route error ...' };
+    const result = new ApiError(error);
+    expect(result.name).to.equal('ApiError');
+    expect(result.message).to.equal('Route error ...');
+    expect(result.error).to.eql(error);
+  });
 
-    it('#isExpiredInSeconds', () => {
-        const nowSecs = Math.ceil(new Date().getTime() / 1000);
-        const result1 = isExpiredInSeconds(nowSecs);
-        const result2 = isExpiredInSeconds(nowSecs + 10);
-        const result3 = isExpiredInSeconds(nowSecs + 10, 10);
-        expect(result1).to.equal(true, 'expire now');
-        expect(result2).to.equal(false, 'expire in 10 secs later');
-        expect(result3).to.equal(true, 'expire in 10 secs later, but also cost 10 secs');
-    });
+  it('#isExpiredInSeconds', () => {
+    const nowSecs = Math.ceil(new Date().getTime() / 1000);
+    const result1 = isExpiredInSeconds(nowSecs);
+    const result2 = isExpiredInSeconds(nowSecs + 10);
+    const result3 = isExpiredInSeconds(nowSecs + 10, 10);
+    expect(result1).to.equal(true, 'expire now');
+    expect(result2).to.equal(false, 'expire in 10 secs later');
+    expect(result3).to.equal(true, 'expire in 10 secs later, but also cost 10 secs');
+  });
 
 });
