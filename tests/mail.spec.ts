f6de8b46057b16f9095c102dca74dfeb43200d5b
Lam Nhan <lamnhan92@gmail.com>
2019-04-17 16:39:20 +0700

    add specs

--- tests/mail.spec.ts
+++ tests/mail.spec.ts
@@ -9,106 +9,106 @@ import { MailService } from '../src/lib/mail/mail.service';
 import { mail } from '../src/lib/mail/index';
 
 const mailService = new MailService(
-    new AppService({ backendUrl: '' }),
+  new AppService({ backendUrl: '' }),
 );
 
 let apiGetStub: sinon.SinonStub;
 let apiPostStub: sinon.SinonStub;
 
 function buildStubs() {
-    // @ts-ignore
-    apiGetStub = sinon.stub(mailService.Api, 'get');
-    // @ts-ignore
-    apiPostStub = sinon.stub(mailService.Api, 'post');
+  // @ts-ignore
+  apiGetStub = sinon.stub(mailService.Api, 'get');
+  // @ts-ignore
+  apiPostStub = sinon.stub(mailService.Api, 'post');
 }
 
 function restoreStubs() {
-    apiGetStub.restore();
-    apiPostStub.restore();
+  apiGetStub.restore();
+  apiPostStub.restore();
 }
 
 describe('(Mail) Mail service', () => {
 
-    beforeEach(() => {
-        buildStubs();
-        apiGetStub.callsFake(async (endpoint, params) => {
-            return { method: 'GET', endpoint, params };
-        });
-        apiPostStub.callsFake(async (endpoint, params, body) => {
-            return { method: 'POST', endpoint, params, body };
-        });
+  beforeEach(() => {
+    buildStubs();
+    apiGetStub.callsFake(async (endpoint, params) => {
+      return { method: 'GET', endpoint, params };
     });
-
-    afterEach(() => restoreStubs());
-
-    it('properties', () => {
-        expect(mailService.app instanceof AppService).to.equal(true);
-        // @ts-ignore
-        expect(mailService.Api instanceof ApiService).to.equal(true);
+    apiPostStub.callsFake(async (endpoint, params, body) => {
+      return { method: 'POST', endpoint, params, body };
     });
+  });
 
-    it('#quota', async () => {
-        const result = await mailService.quota();
-        expect(result).to.eql({
-            method: 'GET',
-            endpoint: '/',
-            params: {},
-        });
-    });
+  afterEach(() => restoreStubs());
 
-    it('#send', async () => {
-        const result = await mailService.send(
-            {
-                recipient: 'xxx@xxx.xxx',
-            },
-            'message',
-            {
-                hello: { name: 'John' },
-            },
-            false,
-        );
-        expect(result).to.eql({
-            method: 'POST',
-            endpoint: '/',
-            params: {},
-            body: {
-                mailingData: {
-                    recipient: 'xxx@xxx.xxx',
-                },
-                category: 'message',
-                template: {
-                    hello: { name: 'John' },
-                },
-                silent: false,
-            },
-        });
+  it('properties', () => {
+    expect(mailService.app instanceof AppService).to.equal(true);
+    // @ts-ignore
+    expect(mailService.Api instanceof ApiService).to.equal(true);
+  });
+
+  it('#quota', async () => {
+    const result = await mailService.quota();
+    expect(result).to.eql({
+      method: 'GET',
+      endpoint: '/',
+      params: {},
     });
+  });
+
+  it('#send', async () => {
+    const result = await mailService.send(
+      {
+        recipient: 'xxx@xxx.xxx',
+      },
+      'message',
+      {
+        hello: { name: 'John' },
+      },
+      false,
+    );
+    expect(result).to.eql({
+      method: 'POST',
+      endpoint: '/',
+      params: {},
+      body: {
+        mailingData: {
+          recipient: 'xxx@xxx.xxx',
+        },
+        category: 'message',
+        template: {
+          hello: { name: 'John' },
+        },
+        silent: false,
+      },
+    });
+  });
 
 });
 
 describe('(Mail) methods', () => {
 
-    it('#mail (no app, no default app)', () => {
-        window['$$$SHEETBASE_APPS'] = null;
-        expect(
-            mail.bind(null),
-        ).to.throw('No app for mail component.');
-    });
+  it('#mail (no app, no default app)', () => {
+    window['$$$SHEETBASE_APPS'] = null;
+    expect(
+      mail.bind(null),
+    ).to.throw('No app for mail component.');
+  });
 
-    it('#mail (no app, default app)', () => {
-        window['$$$SHEETBASE_APPS'] = {
-            getApp: () => ({ Mail: 'An Mail instance' }),
-        };
+  it('#mail (no app, default app)', () => {
+    window['$$$SHEETBASE_APPS'] = {
+      getApp: () => ({ Mail: 'An Mail instance' }),
+    };
 
-        const result = mail();
+    const result = mail();
 
-        expect(result).to.equal('An Mail instance');
-    });
+    expect(result).to.equal('An Mail instance');
+  });
 
-    it('#mail (app has no .Mail)', () => {
-        const result = mail(new AppService({ backendUrl: '' }));
+  it('#mail (app has no .Mail)', () => {
+    const result = mail(new AppService({ backendUrl: '' }));
 
-        expect(result instanceof MailService).to.equal(true);
-    });
+    expect(!!result).to.equal(true);
+  });
 
 });
--- tests/storage.spec.ts
+++ tests/storage.spec.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import * as sinon from 'sinon';
 
+import 'jsdom-global/register';
+
 import { AppService } from '../src/lib/app/app.service';
 import { ApiService } from '../src/lib/api/api.service';
 
@@ -9,99 +11,97 @@ import { StorageService } from '../src/lib/storage/storage.service';
 import { storage } from '../src/lib/storage/index';
 
 const storageService = new StorageService(
-    new AppService({ backendUrl: '' }),
+  new AppService({ backendUrl: '' }),
 );
 
 let apiGetStub: sinon.SinonStub;
 let apiPostStub: sinon.SinonStub;
 
-function buildStubs() {
-    // @ts-ignore
-    apiGetStub = sinon.stub(storageService.Api, 'get');
-    // @ts-ignore
-    apiPostStub = sinon.stub(storageService.Api, 'post');
+function before() {
+  // @ts-ignore
+  apiGetStub = sinon.stub(storageService.Api, 'get');
+  // @ts-ignore
+  apiPostStub = sinon.stub(storageService.Api, 'post');
+  // stub
+  apiGetStub.callsFake(async (endpoint, params) => {
+    return { method: 'GET', endpoint, params };
+  });
+  apiPostStub.callsFake(async (endpoint, params, body) => {
+    return { method: 'POST', endpoint, params, body };
+  });
 }
 
-function restoreStubs() {
-    apiGetStub.restore();
-    apiPostStub.restore();
+function after() {
+  apiGetStub.restore();
+  apiPostStub.restore();
 }
 
 describe('(Storage) Storage service', () => {
 
-    beforeEach(() => {
-        buildStubs();
-        apiGetStub.callsFake(async (endpoint, params) => {
-            return { method: 'GET', endpoint, params };
-        });
-        apiPostStub.callsFake(async (endpoint, params, body) => {
-            return { method: 'POST', endpoint, params, body };
-        });
-    });
-    afterEach(() => restoreStubs());
+  beforeEach(before);
+  afterEach(after);
 
-    it('properties', () => {
-        expect(storageService.app instanceof AppService).to.equal(true);
-        // @ts-ignore
-        expect(storageService.Api instanceof ApiService).to.equal(true);
-    });
+  it('properties', () => {
+    expect(storageService.app instanceof AppService).to.equal(true);
+    // @ts-ignore
+    expect(storageService.Api instanceof ApiService).to.equal(true);
+  });
 
-    it('#info', async () => {
-        const result = await storageService.info('xxx');
-        expect(result).to.eql({
-            method: 'GET',
-            endpoint: '/',
-            params: { id: 'xxx' },
-        });
+  it('#info', async () => {
+    const result = await storageService.info('xxx');
+    expect(result).to.eql({
+      method: 'GET',
+      endpoint: '/',
+      params: { id: 'xxx' },
     });
+  });
 
-    it('#upload', async () => {
-        const result = await storageService.upload({
-            name: 'file1.txt',
-            size: 1000,
-            base64Data: '<base64Data>',
-        }, 'me', 'filex');
-        expect(result).to.eql({
-            method: 'POST',
-            endpoint: '/',
-            params: {},
-            body: {
-                fileResource: {
-                    name: 'file1.txt',
-                    size: 1000,
-                    base64Data: '<base64Data>',
-                },
-                customFolder: 'me',
-                rename: 'filex',
-            },
-        });
+  it('#upload', async () => {
+    const result = await storageService.upload({
+      name: 'file1.txt',
+      size: 1000,
+      base64Data: '<base64Data>',
+    }, 'me', 'filex');
+    expect(result).to.eql({
+      method: 'POST',
+      endpoint: '/',
+      params: {},
+      body: {
+        fileResource: {
+          name: 'file1.txt',
+          size: 1000,
+          base64Data: '<base64Data>',
+        },
+        customFolder: 'me',
+        rename: 'filex',
+      },
     });
+  });
 
 });
 
 describe('(Storage) methods', () => {
 
-    it('#storage (no app, no default app)', () => {
-        window['$$$SHEETBASE_APPS'] = null;
-        expect(
-            storage.bind(null),
-        ).to.throw('No app for storage component.');
-    });
+  it('#storage (no app, no default app)', () => {
+    window['$$$SHEETBASE_APPS'] = null;
+    expect(
+      storage.bind(null),
+    ).to.throw('No app for storage component.');
+  });
 
-    it('#storage (no app, default app)', () => {
-        window['$$$SHEETBASE_APPS'] = {
-            getApp: () => ({ Storage: 'An Storage instance' }),
-        };
+  it('#storage (no app, default app)', () => {
+    window['$$$SHEETBASE_APPS'] = {
+      getApp: () => ({ Storage: 'An Storage instance' }),
+    };
 
-        const result = storage();
+    const result = storage();
 
-        expect(result).to.equal('An Storage instance');
-    });
+    expect(result).to.equal('An Storage instance');
+  });
 
-    it('#storage (app has no .Storage)', () => {
-        const result = storage(new AppService({ backendUrl: '' }));
-
-        expect(result instanceof StorageService).to.equal(true);
-    });
+  it('#storage (app has no .Storage)', () => {
+    const result = storage(new AppService({ backendUrl: '' }));
+    expect(!!result).to.equal(true);
+  });
 
 });
