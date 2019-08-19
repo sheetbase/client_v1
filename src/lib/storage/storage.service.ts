bc39ecf48c73996602ce1d95ff846acd5a22cd57
Lam Nhan <lamnhan92@gmail.com>
2019-08-19 23:23:46 +0700

    update storage service

--- src/lib/storage/storage.service.ts
+++ src/lib/storage/storage.service.ts
@@ -1,56 +1,125 @@
-import { FileResource } from '@sheetbase/drive';
-
 import { AppService } from '../app/app.service';
 import { ApiService } from '../api/api.service';
 
+import {
+  FileReaderResult,
+  FileInfo,
+  UploadFile,
+  UploadResource,
+  RenamePolicy,
+  FileSharing,
+  FileUpdateData,
+} from './types';
+
 export class StorageService {
 
-    private Api: ApiService;
+  private Api: ApiService;
 
-    app: AppService;
+  app: AppService;
 
-    constructor(app: AppService) {
-        this.app = app;
-        this.Api = this.app.Api
-            .extend()
-            .setEndpoint(this.app.options.storageEndpoint || 'storage');
-        // not recommended
-        console.warn('NOT RECOMMENDED: May not use the Storage service now!');
-    }
+  constructor(app: AppService) {
+    this.app = app;
+    this.Api = this.app.Api
+      .extend()
+      .setEndpoint(this.app.options.storageEndpoint || 'storage');
+  }
 
-    async info(fileId: string) {
-        return await this.Api.get('/', { id: fileId });
-    }
+  private isValidType(mimeType: string) {
+    const { storageAllowTypes: allowTypes } = this.app.options;
+    return !allowTypes || allowTypes.indexOf(mimeType) > -1;
+  }
 
-    async upload(
-        fileResource: FileResource,
-        customFolder?: string,
-        rename?: string,
+  private isValidSize(sizeBytes: number) {
+    const { storageMaxSize: maxSize } = this.app.options;
+    const sizeMB = sizeBytes / 1000000;
+    return !maxSize || maxSize === 0 || sizeMB <= maxSize;
+  }
+
+  private getBase64Props(base64Value: string): {
+    mimeType: string;
+    size: number;
+  } {
+    const [ header, body ] = base64Value.split(';base64,');
+    const mimeType = header.replace('data:', '');
+    const size = (body.replace(/\=/g, '').length * 0.75);
+    return { mimeType, size };
+  }
+
+  private checkUploadFile(fileData: UploadFile) {
+    let error: string;
+    // check file data
+    if (
+      !fileData ||
+      !fileData.base64Value ||
+      !fileData.name
     ) {
-        if (!fileResource) {
-            throw new Error('No file resource');
-        }
-        // request body
-        const body: any = { fileResource };
-        if (customFolder) { body.customFolder = customFolder; }
-        if (rename) { body.rename = rename; }
-        return await this.Api.post('/', {}, body);
+      error = 'Missing upload data.';
     }
+    // check type and size
+    const { mimeType, size } = this.getBase64Props(fileData.base64Value);
+    if (
+      !this.isValidType(mimeType) ||
+      !this.isValidSize(size)
+    ) {
+      error = 'Invalid file type or the file is too big.';
+    }
+    // throw error
+    if (!!error) {
+      throw new Error(error);
+    }
+  }
 
-    async load(file: File) {
-        return new Promise((resolve, reject) => {
-            if (!file) {
-                reject('No file.');
-            }
-            // read the file
-            const reader = new FileReader();
-            reader.onload = (e: any) => {
-                const base64Data = e.target.result;
-                const { name, size } = file;
-                resolve({ name, size, base64Data });
-            };
-            reader.readAsDataURL(file);
-        });
+  info(id: string): Promise<FileInfo> {
+    return this.Api.get('/', { id });
+  }
+
+  upload(
+    fileData: UploadFile,
+    customFolder?: string,
+    renamePolicy?: RenamePolicy,
+    sharing: FileSharing = 'PRIVATE',
+  ): Promise<FileInfo> {
+    this.checkUploadFile(fileData);
+    // build the request body
+    const body: any = { file: fileData };
+    if (customFolder) {
+      body.folder = customFolder;
     }
+    if (renamePolicy) {
+      body.rename = renamePolicy;
+    }
+    if (sharing) {
+      body.share = sharing;
+    }
+    return this.Api.put('/', {}, body);
+  }
+
+  uploadMultiple(uploadResources: UploadResource[]): Promise<FileInfo[]> {
+    for (let i = 0; i < uploadResources.length; i++) {
+      const { file: fileData } = uploadResources[i];
+      this.checkUploadFile(fileData);
+    }
+    return this.Api.put('/', {}, { files: uploadResources });
+  }
+
+  update(id: string, data: FileUpdateData): Promise<{ done: true }> {
+    return this.Api.post('/', {}, { id, data });
+  }
+
+  remove(id: string): Promise<{ done: true }> {
+    return this.Api.delete('/', {}, { id });
+  }
+
+  read(file: File): Promise<FileReaderResult> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const base64Value = e.target.result;
+        const { name, size } = file;
+        resolve({ name, size, base64Value });
+      };
+      reader.readAsDataURL(file);
+    });
+  }
 
 }
--- src/lib/storage/types.ts
+++ src/lib/storage/types.ts
@@ -1,9 +1,53 @@
-export interface StorageOptions {
-    storageEndpoint?: string;
+export interface StorageOptions extends StorageLimitOptions {
+  storageEndpoint?: string;
 }
 
-export interface FileResource {
-    name: string;
-    base64Data: string;
-    size?: number;
+export interface StorageLimitOptions {
+  storageAllowTypes?: string[]; // mimetype list
+  storageMaxSize?: number; // size limit
+}
+
+export interface FileReaderResult {
+  name: string;
+  size: number;
+  base64Value: string;
+}
+
+export interface UploadFile {
+  name: string;
+  base64Value: string;
+}
+
+export interface UploadResource {
+  file: UploadFile;
+  folder?: string;
+  rename?: RenamePolicy;
+  share?: FileSharing;
+}
+
+export interface FileInfo {
+  id: string;
+  name: string;
+  mimeType: string;
+  description: string;
+  size: number;
+  link: string;
+  url: string;
+  downloadUrl: string;
+}
+
+export type RenamePolicy = 'AUTO' | 'HASH';
+
+export type FileSharing = SharingPreset | SharingConfig;
+export type SharingPreset = 'PUBLIC' | 'PRIVATE';
+export interface SharingConfig {
+  access?: string;
+  permission?: string;
+}
+
+export interface FileUpdateData {
+  name?: string;
+  description?: string;
+  sharing?: FileSharing;
+  content?: string; // text file only
 }
